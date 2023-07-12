2944396b80553adc0a45abf94d685819b5262135
Maestro1982 <kris.tote@telenet.be>
2023-07-12 16:36:15 +0200

    modify layout.tsx in app folder, create type.ts file, create get-categories.tsx in actions folder, main-nav.tsx and navbar.tsx in components folder, container.tsx in components uit folder, utils.ts in lib folder

---
+++ actions/get-categories.tsx
@@ -0,0 +1,15 @@
+import { Category } from '@/types';
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
+
+const getCategories = async (): Promise<Category[]> => {
+  const res = await fetch(URL, {
+    headers: {
+      'cache-control': 'no-cache',
+    },
+  });
+
+  return res.json();
+};
+
+export default getCategories;
