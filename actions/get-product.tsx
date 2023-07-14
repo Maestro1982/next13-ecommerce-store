70d65e13b0ed62d7dabbb5d4232fff58fc22d639
Maestro1982 <kris.tote@telenet.be>
2023-07-14 21:37:01 +0200

    modify get-product.tsx in actions folder and navbar.tsx in components folder

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -3,14 +3,7 @@ import { Product } from '@/types';
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
 const getProduct = async (id: string): Promise<Product> => {
-  const cacheBuster = new Date().getTime(); // Unique value for cache-busting
-  const urlWithCacheBuster = `${URL}/${id}?_=${cacheBuster}`; // Append cache-buster to the URL
-
-  const res = await fetch(urlWithCacheBuster, {
-    headers: {
-      'Cache-Control': 'no-store',
-    },
-  });
+  const res = await fetch(`${URL}/${id}`);
 
   return res.json();
 };
