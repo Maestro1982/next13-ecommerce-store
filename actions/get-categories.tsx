b52cdd3b41db6c673c585761095deaf371551fc4
Maestro1982 <kris.tote@telenet.be>
2023-07-18 11:58:44 +0200

    modify get-categories.tsx in actions folder, create get-category.tsx, get-colors.tsx, get-sizes.tsx in actions folder, create page.tsx in app routes category categoryId folder, filter.tsx in app routes category categoryId components folder, mobile-filters.tsx in app routes category categoryId components folder, modify page.tsx in app routes product productId folder, button.tsx in components ui folder, create info.tsx in components folder

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -3,14 +3,7 @@ import { Category } from '@/types';
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-  const cacheBuster = new Date().getTime(); // Unique value for cache-busting
-  const urlWithCacheBuster = `${URL}?_=${cacheBuster}`; // Append cache-buster to the URL
-
-  const res = await fetch(urlWithCacheBuster, {
-    headers: {
-      'Cache-Control': 'no-store',
-    },
-  });
+  const res = await fetch(URL);
 
   return res.json();
 };
